7ad294de35cb5872f69b4b60583cbb99d3e79b0f
Eduardo Lima <eduardolimacrr@gmail.com>
2024-05-01 00:27:05 -0300

    dia-4

---
+++ index.js
@@ -0,0 +1,9 @@
+let idade = Number(prompt('insira a sua idade'))
+
+if(idade <= 10){
+    console.log(idade)
+    console.log('voce tem menos ou tem 10 anos')
+}else{
+    console.log('voce tem mais de 10 anos')
+}
+
